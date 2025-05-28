b67c4c74fee6cd5b1f32ffd9c0955cb912112ea3
mykytiukartem <mikituka06@gmail.com>
2025-05-28 16:54:42 +0300

    6 лаб

--- 1.5/1.5.cpp
+++ 1.5/1.5.cpp
@@ -1,160 +1,148 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <ctime>
-#include <fstream>
 using namespace std;
 
-class A {
-protected:
+// ===== Ієрархія 1: Без віртуального успадкування =====
+class A1 {
+public:
     int a;
-public:
-    A() : a(rand() % 100) { cout << "A created\n"; }
-    virtual ~A() { cout << "A destroyed\n"; }
-    virtual void print() const { cout << "A: " << a << endl; }
+    A1() { cout << "A1 created\n"; }
+    ~A1() { cout << "A1 destroyed\n"; }
 };
 
-class B {
-protected:
+class B1 {
+public:
     int b;
-public:
-    B() : b(rand() % 100) { cout << "B created\n"; }
-    virtual ~B() { cout << "B destroyed\n"; }
-    virtual void print() const { cout << "B: " << b << endl; }
+    B1() { cout << "B1 created\n"; }
+    ~B1() { cout << "B1 destroyed\n"; }
 };
 
-// -------- Без віртуального успадкування --------
-class C1 : public A, public B {
-protected:
+class C1 : public A1, public B1 {
+public:
     int c;
-public:
-    C1() : c(rand() % 100) { cout << "C1 created\n"; }
-    ~C1() override { cout << "C1 destroyed\n"; }
-    void print() const override {
-        A::print();
-        B::print();
-        cout << "C1: " << c << endl;
-    }
+    C1() { cout << "C1 created\n"; }
+    ~C1() { cout << "C1 destroyed\n"; }
 };
 
-class D1 : public C1 {
-protected:
+class D1 : public A1 {
+public:
     int d;
-public:
-    D1() : d(rand() % 100) { cout << "D1 created\n"; }
-    ~D1() override { cout << "D1 destroyed\n"; }
-    void print() const override {
-        C1::print();
-        cout << "D1: " << d << endl;
-    }
+    D1() { cout << "D1 created\n"; }
+    ~D1() { cout << "D1 destroyed\n"; }
 };
 
-class E1 : public C1 {
-protected:
+class E1 : public B1 {
+public:
     int e;
-public:
-    E1() : e(rand() % 100) { cout << "E1 created\n"; }
-    ~E1() override { cout << "E1 destroyed\n"; }
-    void print() const override {
-        C1::print();
-        cout << "E1: " << e << endl;
-    }
+    E1() { cout << "E1 created\n"; }
+    ~E1() { cout << "E1 destroyed\n"; }
 };
 
-class F1 : public D1, public E1 {
-protected:
+class F1 : public C1, public D1, public E1 {
+public:
     int f;
-public:
-    F1() : f(rand() % 100) { cout << "F1 created\n"; }
-    ~F1() override { cout << "F1 destroyed\n"; }
-    void print() const {
-        D1::print(); // двозначність!
-        E1::print();
-        cout << "F1: " << f << endl;
-    }
+    F1() { cout << "F1 created\n"; }
+    ~F1() { cout << "F1 destroyed\n"; }
 };
 
-// -------- З віртуальним успадкуванням --------
-class AV {
-protected:
+// ===== Ієрархія 2: З віртуальним успадкуванням =====
+class A2 {
+public:
     int a;
-public:
-    AV() : a(rand() % 100) { cout << "AV created\n"; }
-    virtual ~AV() { cout << "AV destroyed\n"; }
-    virtual void print() const { cout << "AV: " << a << endl; }
+    A2() { cout << "A2 created\n"; }
+    virtual ~A2() { cout << "A2 destroyed\n"; }
 };
 
-class BV {
-protected:
+class B2 {
+public:
     int b;
-public:
-    BV() : b(rand() % 100) { cout << "BV created\n"; }
-    virtual ~BV() { cout << "BV destroyed\n"; }
-    virtual void print() const { cout << "BV: " << b << endl; }
+    B2() { cout << "B2 created\n"; }
+    virtual ~B2() { cout << "B2 destroyed\n"; }
 };
 
-class CV : public virtual AV, public virtual BV {
-protected:
+class C2 : virtual public A2, virtual public B2 {
+public:
     int c;
-public:
-    CV() : c(rand() % 100) { cout << "CV created\n"; }
-    ~CV() override { cout << "CV destroyed\n"; }
-    void print() const override {
-        AV::print();
-        BV::print();
-        cout << "CV: " << c << endl;
-    }
+    C2() { cout << "C2 created\n"; }
+    virtual ~C2() { cout << "C2 destroyed\n"; }
 };
 
-class DV : public virtual CV {
-protected:
+class D2 : virtual public A2 {
+public:
     int d;
-public:
-    DV() : d(rand() % 100) { cout << "DV created\n"; }
-    ~DV() override { cout << "DV destroyed\n"; }
-    void print() const override {
-        CV::print();
-        cout << "DV: " << d << endl;
-    }
+    D2() { cout << "D2 created\n"; }
+    virtual ~D2() { cout << "D2 destroyed\n"; }
 };
 
-class EV : public virtual CV {
-protected:
+class E2 : virtual public B2 {
+public:
     int e;
-public:
-    EV() : e(rand() % 100) { cout << "EV created\n"; }
-    ~EV() override { cout << "EV destroyed\n"; }
-    void print() const override {
-        CV::print();
-        cout << "EV: " << e << endl;
-    }
+    E2() { cout << "E2 created\n"; }
+    virtual ~E2() { cout << "E2 destroyed\n"; }
 };
 
-class FV : public DV, public EV {
-protected:
+class F2 : public C2, public D2, public E2 {
+public:
     int f;
-public:
-    FV() : f(rand() % 100) { cout << "FV created\n"; }
-    ~FV() override { cout << "FV destroyed\n"; }
-    void print() const {
-        DV::print();
-        EV::print();
-        cout << "FV: " << f << endl;
-    }
+    F2() { cout << "F2 created\n"; }
+    virtual ~F2() { cout << "F2 destroyed\n"; }
 };
 
-// -------- Тестування --------
+// ===== Введення даних =====
+int inputValue(const string& source) {
+    if (source == "random") {
+        return rand() % 100;
+    } else if (source == "keyboard") {
+        int val;
+        cout << "Введіть значення: ";
+        cin >> val;
+        return val;
+    } else {
+        ifstream file(source);
+        int val;
+        if (file >> val) return val;
+        else return -1;
+    }
+}
+
+// ===== Основна програма =====
 int main() {
     srand(time(0));
-
-    cout << "\n--- F1 (без віртуального успадкування) ---\n";
+    cout << "=== Створення F1 (без virtual inheritance) ===\n";
     F1 f1;
-    f1.print();
-    cout << "Size F1: " << sizeof(f1) << " bytes\n";
+    cout << "=== Створення F2 (з virtual inheritance) ===\n";
+    F2 f2;
 
-    cout << "\n--- FV (з віртуальним успадкуванням) ---\n";
-    FV fv;
-    fv.print();
-    cout << "Size FV: " << sizeof(fv) << " bytes\n";
+    cout << "\n=== Розміри об'єктів ===\n";
+    cout << "sizeof(A1): " << sizeof(A1) << endl;
+    cout << "sizeof(B1): " << sizeof(B1) << endl;
+    cout << "sizeof(C1): " << sizeof(C1) << endl;
+    cout << "sizeof(D1): " << sizeof(D1) << endl;
+    cout << "sizeof(E1): " << sizeof(E1) << endl;
+    cout << "sizeof(F1): " << sizeof(F1) << endl;
+
+    cout << "sizeof(A2): " << sizeof(A2) << endl;
+    cout << "sizeof(B2): " << sizeof(B2) << endl;
+    cout << "sizeof(C2): " << sizeof(C2) << endl;
+    cout << "sizeof(D2): " << sizeof(D2) << endl;
+    cout << "sizeof(E2): " << sizeof(E2) << endl;
+    cout << "sizeof(F2): " << sizeof(F2) << endl;
+
+    cout << "\n=== Тест введення значень ===\n";
+    int r = inputValue("random");
+    cout << "Random value: " << r << endl;
+
+    int k = inputValue("keyboard");
+    cout << "Keyboard value: " << k << endl;
+
+    ofstream outFile("input.txt");
+    outFile << 42;
+    outFile.close();
+
+    int f = inputValue("input.txt");
+    cout << "From file: " << f << endl;
 
     return 0;
 }
